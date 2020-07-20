e97722b18c06c79de9ed79a1412c830b65595540
Sagynbek <kenzhebaevsagynbek@gmail.com>
2020-07-20 13:09:05 +0300

    Mongoose usage of cache implemented

---
+++ services/cache.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
+
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+
+client.hget = util.promisify(client.hget);
+
+const execOriginal = mongoose.Query.prototype.exec;
+
+
+
+mongoose.Query.prototype.cache = function (options = {}) {
+  this.useCache = true;
+  this.hashKey = JSON.stringify(options.key || "");
+
+  return this;
+}
+
+mongoose.Query.prototype.exec = async function () {
+  if (!this.useCache) {
+    return execOriginal.apply(this, arguments);
+  }
+
+  const key = JSON.stringify(Object.assign({}, this.getQuery(), {
+    collection: this.mongooseCollection.name
+  }));
+
+  const cacheValue = await client.hget(this.hashKey, key);
+
+  if (cacheValue) {
+    console.log("Using cache value");
+    const doc = JSON.parse(cacheValue);
+
+    return Array.isArray(doc)
+      ? doc.map(item => new this.model(item))
+      : new this.model(doc);
+  }
+
+  const result = await execOriginal.apply(this, arguments);
+
+  client.hset(this.hashKey, key, JSON.stringify(result), () => { });
+
+  return result;
+}
+
+
+function clearHash(hashKey) {
+  client.del(JSON.stringify(hashKey));
+}
+
+module.exports = {
+  clearHash,
+}
