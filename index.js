9680cb4003f2f81395bde6f7749ad117a2992739
Sagynbek <kenzhebaevsagynbek@gmail.com>
2020-07-21 09:26:58 +0300

    CI trigger

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ app.use(
     keys: [keys.cookieKey]
   })
 );
+
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -41,6 +42,7 @@ if (['production', 'ci'].includes(process.env.NODE_ENV)) {
     res.sendFile(path.resolve('client', 'build', 'index.html'));
   });
 }
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
