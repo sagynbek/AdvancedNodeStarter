afa55f7af85dc720f8e497f8b78a3a69df86ed4f
Sagynbek <kenzhebaevsagynbek@gmail.com>
2020-07-21 09:21:47 +0300

    Travis CI setup for testing

--- config/ci.js
+++ config/ci.js
@@ -1,7 +1,7 @@
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
-  cookieKey: "123321123321123",
+  mongoURI: '',
+  cookieKey: process.env.COOKIE_KEY,
   redisUrl: "redis://127.0.0.1:6379"
 };
