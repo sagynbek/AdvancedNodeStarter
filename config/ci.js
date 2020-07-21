40635141a276f9b5ac75f73312aeb831f0a97398
Sagynbek <kenzhebaevsagynbek@gmail.com>
2020-07-21 09:40:13 +0300

    CookieKey set for Travis CI

--- config/ci.js
+++ config/ci.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID: '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
   mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
-  cookieKey: process.env.COOKIE_KEY,
+  cookieKey: "123321123321123",
   redisUrl: "redis://127.0.0.1:6379"
 };
--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
-  cookieKey: "123321123321123",
+  cookieKey: process.env.COOKIE_KEY,
   redisUrl: "redis://127.0.0.1:6379"
 };
