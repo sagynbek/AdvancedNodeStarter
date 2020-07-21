eb0b3e3f7d3ccedd801de65c0bd58d4cefc970d4
Sagynbek <kenzhebaevsagynbek@gmail.com>
2020-07-21 09:35:34 +0300

    Google IDs added to CI

--- config/ci.js
+++ config/ci.js
@@ -1,6 +1,6 @@
 module.exports = {
-  googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  googleClientID: '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
+  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
   mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
   cookieKey: process.env.COOKIE_KEY,
   redisUrl: "redis://127.0.0.1:6379"
